3a49ea48ae24970b1bd6360acba08119094f9d2c
Sol Astrius (Daniel Gorlov) <me@danielsol.dev>
2026-01-09 17:05:34 +0100

    Add static musl builds and release artifacts

--- flake.nix
+++ flake.nix
@@ -15,40 +15,34 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
+
+        commonArgs = {
           pname = "aw-clickhouse-bridge";
           version = "0.1.0";
-
           src = ./.;
-
           cargoLock = {
             lockFile = ./Cargo.lock;
             outputHashes = {
               "aw-models-0.1.0" = "sha256-X9DNYAWcY0yFLwv62OGDp01AaGsDCtElkn5sAFmZkyI=";
             };
           };
-
           meta = with pkgs.lib; {
             description = "ActivityWatch to ClickHouse bridge";
             homepage = "https://github.com/SolAstrius/aw-clickhouse-bridge";
             license = licenses.mit;
-            maintainers = [ ];
             mainProgram = "aw-clickhouse-bridge";
           };
+        };
+      in
+      {
+        packages = {
+          default = pkgs.rustPlatform.buildRustPackage commonArgs;
+          static = pkgs.pkgsStatic.rustPlatform.buildRustPackage commonArgs;
         };
 
         devShells.default = pkgs.mkShell {
           inputsFrom = [ self.packages.${system}.default ];
-
-          packages = with pkgs; [
-            cargo
-            rustc
-            rust-analyzer
-            clippy
-            rustfmt
-          ];
+          packages = with pkgs; [ cargo rustc rust-analyzer clippy rustfmt ];
         };
       }
     );
