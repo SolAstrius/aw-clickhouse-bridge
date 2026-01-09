a893c5876e7c692003c9330c899b9128b903a22f
Sol Astrius (Daniel Gorlov) <me@danielsol.dev>
2026-01-09 16:42:27 +0100

    Add Nix flake for packaging
    - flake.nix with rustPlatform.buildRustPackage
    - Cachix integration (solastrius cache)
    - GitHub Actions workflow for CI and cache push
    - Track Cargo.lock for reproducible builds

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "ActivityWatch to ClickHouse bridge";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://solastrius.cachix.org" ];
+    extra-trusted-public-keys = [ "solastrius.cachix.org-1:MawFli42h9VuWjlURZvxDG+M/tfUbELRwU+QN/6VvdM=" ];
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "aw-clickhouse-bridge";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "aw-models-0.1.0" = "sha256-X9DNYAWcY0yFLwv62OGDp01AaGsDCtElkn5sAFmZkyI=";
+            };
+          };
+
+          meta = with pkgs.lib; {
+            description = "ActivityWatch to ClickHouse bridge";
+            homepage = "https://github.com/SolAstrius/aw-clickhouse-bridge";
+            license = licenses.mit;
+            maintainers = [ ];
+            mainProgram = "aw-clickhouse-bridge";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+
+          packages = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            clippy
+            rustfmt
+          ];
+        };
+      }
+    );
+}
